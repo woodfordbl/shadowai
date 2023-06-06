7586f4e07909b2c3c39454f5ed537e79c1d7577f
woodfordbl <blakewoodford@college.harvard.edu>
2023-06-06 13:04:56 -0700

    added serverside features?

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve the static files
+app.use('/client', express.static('path/to/client'));
+
 // Define routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'client/assets/icon-128.png');
+  res.send("Hello World!");
 });
 
 // Start the server
