9ac13bacf2bb8d11198aeecdbca4ee4fa60f8964
woodfordbl <blakewoodford@college.harvard.edu>
2023-06-06 13:20:28 -0700

    further updated paths

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3000;
 
 // Serve the static files
 app.use('/client', express.static('client'));
+app.use('/client', express.static('client/src/taskpane'));
+app.use('/client', express.static('client/assets'));
+
 
 // Define routes
 app.get('/', (req, res) => {
